a3deb2d98de3253d3e1d9afc52291c16e782e9d0
Cal Henderson <cal@iamcal.com>
2013-12-17 13:48:25 -0800

    switched homepage to use a template

--- index.php
+++ index.php
@@ -4,31 +4,32 @@
 
 	load_plugins();
 	load_data();
-?>
 
-<h1>Slackware</h1>
 
-<b>Add a new service</b>
+	$services = array();
+	foreach (array_keys($plugins_services) as $k){
+		$temp = new $k();
+		$temp->id = $k;
+		$services[] = $temp;
+	}
+	$smarty->assign('services', $services);
 
-<ul>
-<?php foreach (array_keys($plugins_services) as $k){ $plugin = new $k(); ?>
-	<li><a href="add.php?id=<?php echo urlencode($k); ?>"><?php echo $plugin->name; ?></a> - <?php echo $plugin->desc; ?> </li>
-<?php } ?>
-</ul>
+	$auth = array();
+	foreach (array_keys($plugins_auth) as $k){
+		$temp = new $k();
+		$temp->id = $k;
+		$auth[] = $temp;
+	}
+	$smarty->assign('auth', $auth);
 
-<b>Existing services</b>
+	$instances = array();
+	foreach ($data['instances'] as $k => $instance){
+		$instances[] = array(
+			'iid' => $k,
+			'class' => $instance['plugin'],
+		);
+	}
+	$smarty->assign('instances', $instances);
 
-<ul>
-<?php foreach ($data['instances'] as $k => $instance){ ?>
-	<li><a href="edit.php?id=<?php echo urlencode($k); ?>"><?php echo $instance['plugin'].' '.$k; ?></a></li>
-<?php } ?>
-</ul>
 
-<b>Things to configure</b>
-
-<ul>
-<?php foreach (array_keys($plugins_auth) as $k){ $plugin = new $k(); ?>
-	<li><a href="auth.php?id=<?php echo urlencode($k); ?>"><?php echo $plugin->name; ?></a> - <?php echo $plugin->desc; ?> </li>
-<?php } ?>
-</ul>
-
+	$smarty->display('page_index.txt');
