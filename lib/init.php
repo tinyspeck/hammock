d04182a276541518a1d514ca86af5b011b26d6d1
Milo Watanabe <milo@Milos-MacBook-Air.local>
2014-08-13 11:44:09 -0700

    back

--- lib/init.php
+++ lib/init.php
@@ -174,7 +174,6 @@
 
 		if (!strpos($GLOBALS['cfg']['cookie_domain'], '.')){
 			$GLOBALS['smarty']->assign('bad_cookie_domain', 1);
-			//$GLOBALS['smarty']->assign('cookie_domain'), $GLOBALS['cfg']['cookie_domain']);
 		}
 
 		$GLOBALS['smarty']->assign('oauth_url', $oauth_url);
