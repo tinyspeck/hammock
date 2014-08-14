27f93eb6c7f0b532a68003f8fca37354babdb0b2
Nolan Caudill <nolan@nolan-slacktop.local>
2014-08-14 11:25:51 -0700

    Fixes spacing

--- plugins/yo/plugin.php
+++ plugins/yo/plugin.php
@@ -1,67 +1,67 @@
 <?php
 
-	class yo extends SlackServicePlugin {
+class yo extends SlackServicePlugin {
 
-		public $name = "Yo";
-		public $desc = "Zero character communication.";
+    public $name = "Yo";
+    public $desc = "Zero character communication.";
 
-		public $cfg = array(
-        	'has_token' => true,
-    	);
+    public $cfg = array(
+        'has_token' => true,
+    );
 
-		function onInit() {
-			$channels = $this->getChannelsList();
+    function onInit() {
+        $channels = $this->getChannelsList();
 
-	        foreach ($channels as $k => $v) {
-	            if ($v == '#general') {
-	                $this->icfg['channel']      = $k;
-	                $this->icfg['channel_name'] = $v;
-	            }
-	        }
+        foreach ($channels as $k => $v) {
+            if ($v == '#general') {
+                $this->icfg['channel']      = $k;
+                $this->icfg['channel_name'] = $v;
+            }
+        }
 
-			$this->icfg['botname'] = "Yo";
-			$this->icfg['icon_url'] = $GLOBALS['cfg']['root_url'] . 'plugins/yo/icon_48.png';
-		}
+        $this->icfg['botname'] = "Yo";
+        $this->icfg['icon_url'] = $GLOBALS['cfg']['root_url'] . 'plugins/yo/icon_48.png';
+    }
 
-		function onView() {
-			return $this->smarty->fetch('view.txt');
-		}
+    function onView() {
+        return $this->smarty->fetch('view.txt');
+    }
 
-		function onEdit() {
-			$channels = $this->getChannelsList();
-			if ($_GET['save']) {
-	            $this->icfg['channel']      = $_POST['channel'];
-	            $this->icfg['channel_name'] = $channels[$_POST['channel']];
-	            $this->icfg['botname']      = $_POST['botname'];
-	            $this->saveConfig();
+    function onEdit() {
+        $channels = $this->getChannelsList();
+        if ($_GET['save']) {
+            $this->icfg['channel']      = $_POST['channel'];
+            $this->icfg['channel_name'] = $channels[$_POST['channel']];
+            $this->icfg['botname']      = $_POST['botname'];
+            $this->saveConfig();
 
-	            header("location: {$this->getViewUrl()}&saved=1");
-	            exit;
-        	}
-        	$this->smarty->assign('channels', $channels);
-        	return $this->smarty->fetch('edit.txt');
-		}
+            header("location: {$this->getViewUrl()}&saved=1");
+            exit;
+        }
+        $this->smarty->assign('channels', $channels);
+        return $this->smarty->fetch('edit.txt');
+    }
 
-		function onHook($request) {
+    function onHook($request) {
 
-			if (!$request['get']['username']){
-				return array('ok' => false, 'error' => "Invalid payload");
-			}
+        if (!$request['get']['username']){
+            return array('ok' => false, 'error' => "Invalid payload");
+        }
 
-			if (!$this->icfg['channel']) {
-            	return array('ok' => false, 'error' => "No channel configured");
-        	}
+        if (!$this->icfg['channel']) {
+            return array('ok' => false, 'error' => "No channel configured");
+        }
 
-        	$message = 'Yo from *'.$request['get']['username'].'*';
+        $message = 'Yo from *'.$request['get']['username'].'*';
 
-        	$this->postToChannel($message, array(
-        		'channel'	=> $this->icfg['channel'],
-        		'username'	=> $this->icfg['botname'],
-        		'icon_url'	=> $this->icfg['icon_url'],
-        	));
-		}
+        $this->postToChannel($message, array(
+            'channel'	=> $this->icfg['channel'],
+            'username'	=> $this->icfg['botname'],
+            'icon_url'	=> $this->icfg['icon_url'],
+        ));
+    }
 
-		function getLabel() {
-			return "Post notifications to {$this->icfg['channel_name']} as {$this->icfg['botname']}";
-		}
-	}
+    function getLabel() {
+        return "Post notifications to {$this->icfg['channel_name']} as {$this->icfg['botname']}";
+    }
+}
