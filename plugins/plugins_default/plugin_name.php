c354329342d5da3f3f774313b635aee76c13f4c4
Milo Watanabe <milo@Milos-MacBook-Air.local>
2014-09-02 17:08:48 -0700

    default plugin files

---
+++ plugins/plugins_default/plugin_name.php
@@ -0,0 +1,9 @@
+<?php
+
+        class MyServicePlugin extends SlackServicePlugin {
+
+                const NAME = "My Awesome Service";
+                const DESC = "This is what it does, yo";
+                const TOOLTIP = "It gives you info when you need it?";
+                const DEFAULT_BOT_NAME = "MASbot";
+        }
